06982571a1e42600b24c1819605b64c9f4fc6250
Aju Chacko <ajuchacko91@gmail.com>
2023-04-14 17:45:10 +0530

    Create Testcase.php

---
+++ tests/Api/Testcase.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Paypal\Tests\Api;
+
+use Paypal\Client;
+use PHPUnit\Framework\TestCase as PhpUnit;
+use Psr\Http\Client\ClientInterface;
+
+abstract class TestCase extends PhpUnit
+{
+    /**
+     * @return string
+     */
+    abstract protected function getApiClass();
+
+    /**
+     * @param array $methods
+     *
+     * @return \PHPUnit\Framework\MockObject\MockObject
+     */
+    protected function getApiMock(array $methods = [])
+    {
+        $httpClient = $this->getMockBuilder(ClientInterface::class)
+            ->onlyMethods(['sendRequest'])
+            ->getMock();
+        $httpClient
+            ->expects($this->any())
+            ->method('sendRequest');
+
+        $client = Client::createWithHttpClient($httpClient);
+
+        return $this->getMockBuilder($this->getApiClass())
+            ->onlyMethods(\array_merge(['getAsResponse', 'get', 'post', 'delete', 'put'], $methods))
+            ->setConstructorArgs([$client, null])
+            ->getMock();
+    }
+}
